336cfd3e1e433785334b471852447d7e2d29ac1d
Tim <code@timid.nl>
2021-12-01 22:49:28 +0100

    general solution for part 2

--- 2021/day_1/src/main.rs
+++ 2021/day_1/src/main.rs
@@ -1,8 +1,6 @@
 use std::env;
 use std::fs;
 
-//const PUZZLE_INPUT: &str = "input";
-
 fn read_file_to_string() -> String {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
@@ -15,17 +13,30 @@ fn read_file_to_string() -> String {
         .expect("Something went wrong reading the file")
 }
 
-fn calculate_measurements(sequential_depths: String) {
-    let mut sequential_depths_iterator = sequential_depths.split_whitespace();
+fn to_int(o: &Option<&str>) -> u32 {
+    o.unwrap().parse().unwrap()
+}
+
+fn calculate_measurements_sliding_window(sequential_depths: String, window_size: u32) {
+    let mut sequential_depths_iterator = sequential_depths.split_whitespace().peekable();
     let mut increasing_depth_counter = 0;
-    let mut previous_depth: u32 = sequential_depths_iterator
-        .next().unwrap()
-        .parse().unwrap();
-    for depth in sequential_depths_iterator {
-        println!("{}", depth);
-        let current_depth: u32 = depth.parse().unwrap();
-        increasing_depth_counter = increasing_depth_counter + (current_depth > previous_depth) as u32;
-        previous_depth = current_depth;
+    let mut depths_window = Vec::new();
+    //sequential_depths_iterator.next();
+    for _i in 0..window_size {
+        if !sequential_depths_iterator.peek().is_some() {
+            panic!("There are insufficient items available for measuring the depth");
+        }
+        depths_window.push(to_int(&sequential_depths_iterator.next()));
+    }
+    // Keep iterating until the iterator is empty
+    while sequential_depths_iterator.peek().is_some() {
+        let previous_depths_sum: u32 = depths_window.iter().sum();
+        // Remove first added item
+        depths_window.remove(0);
+        depths_window.push(to_int(&sequential_depths_iterator.next()));
+        let current_depths_sum: u32 = depths_window.iter().sum();
+        
+        increasing_depth_counter = increasing_depth_counter + (current_depths_sum > previous_depths_sum) as u32;        
     }
     println!("Depth increased {} times", increasing_depth_counter);
 }
@@ -33,7 +44,5 @@ fn calculate_measurements(sequential_depths: String) {
 fn main() {
     let sequential_depths = read_file_to_string();
 
-    calculate_measurements(sequential_depths);
-
-    //println!("With text:\n{}", contents);
+    calculate_measurements_sliding_window(sequential_depths, 3);
 }
