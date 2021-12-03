1231e1bce3e975a4023badf2f682239be9e0e81c
Tim <code@timid.nl>
2021-12-03 20:30:34 +0100

    and part 2

--- 2021/day_2/src/main.rs
+++ 2021/day_2/src/main.rs
@@ -1,12 +1,6 @@
 use std::env;
 use util::input_operations::{read_file_to_string,split_lines,split_whitespace};
 
-enum Direction {
-    Up,
-    Down,
-    Forward
-}
-
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
@@ -17,22 +11,26 @@ fn main() {
     determine_position(positions);
 }
 
-fn get_direction_and_speed(position: std::string::String) -> (std::string::String, i32) {
+fn get_direction_and_speed(position: std::string::String) -> (std::string::String, u32) {
     let mut position_split = split_whitespace(&position);
     let direction = position_split.next().unwrap().to_string();
-    let speed: i32 = position_split.next().unwrap().parse().unwrap();
+    let speed: u32 = position_split.next().unwrap().parse().unwrap();
     return (direction, speed)
 }
 
 fn determine_position(file_result: String) {
+    let mut aim = 0;
     let mut depth = 0;
     let mut horizontal_position = 0;
     for position in split_lines(&file_result) {
         let (direction, speed) = get_direction_and_speed(position.to_string());
         match direction.as_ref() {
-            "forward" => horizontal_position += speed,
-            "up" => depth -= speed,
-            "down" => depth += speed,
+            "forward" => {
+                horizontal_position += speed;
+                depth += speed * aim
+            },
+            "up" => aim -= speed,
+            "down" => aim += speed,
             _ => panic!("Invalid direction {}. It should be one of the following: 'forward', 'up', 'down'", direction)
         }
     }
