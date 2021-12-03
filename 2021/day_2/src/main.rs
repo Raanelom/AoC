d6eba7b513c8dd73fc2814c52e315ac2ba4cf3ae
Tim <code@timid.nl>
2021-12-03 20:26:28 +0100

    finish day 2 part 1

--- 2021/day_2/src/main.rs
+++ 2021/day_2/src/main.rs
@@ -1,6 +1,12 @@
 use std::env;
 use util::input_operations::{read_file_to_string,split_lines,split_whitespace};
 
+enum Direction {
+    Up,
+    Down,
+    Forward
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
@@ -11,10 +17,26 @@ fn main() {
     determine_position(positions);
 }
 
+fn get_direction_and_speed(position: std::string::String) -> (std::string::String, i32) {
+    let mut position_split = split_whitespace(&position);
+    let direction = position_split.next().unwrap().to_string();
+    let speed: i32 = position_split.next().unwrap().parse().unwrap();
+    return (direction, speed)
+}
+
 fn determine_position(file_result: String) {
+    let mut depth = 0;
+    let mut horizontal_position = 0;
     for position in split_lines(&file_result) {
-        for position in split_whitespace(&String::from(position)) {
-            println!("{}", position);
+        let (direction, speed) = get_direction_and_speed(position.to_string());
+        match direction.as_ref() {
+            "forward" => horizontal_position += speed,
+            "up" => depth -= speed,
+            "down" => depth += speed,
+            _ => panic!("Invalid direction {}. It should be one of the following: 'forward', 'up', 'down'", direction)
         }
     }
+    println!("{}", depth);
+    println!("{}", horizontal_position);
+    println!("The amazing multiplier returns {}", depth * horizontal_position);
 }
