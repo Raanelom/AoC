ad0b757317acfbbc57ee33da8985b219a6b23afe
Tim <code@timid.nl>
2021-12-04 16:34:19 +0100

    finish part 2 of day 3

--- 2021/day_3/src/main.rs
+++ 2021/day_3/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use util::input_operations::{read_file_to_string,split_lines};
 
+#[derive(Clone)]
 struct DiagnosticReport {
     items: Vec<usize>
 }
@@ -17,9 +18,13 @@ fn main() {
         panic!("Expected a filename as argument");
     }
     let input = read_file_to_string(&args[1]);
+    // Determine the size of a single line (number of bits)
     let entry_size = split_lines(&input).next().map(|x| x.len()).unwrap();
     let diagnostic_report = to_report(input);
-    reduce(diagnostic_report, entry_size);
+    let (epsilon_rate, gamma_rate) = calculate_power_consumption(diagnostic_report.clone(), entry_size);
+    println!("The answer is {}", epsilon_rate*gamma_rate);
+
+    calculate_life_support_rating(diagnostic_report, entry_size);
 }
 
 fn to_report(diagnostic_report: std::string::String) -> DiagnosticReport {
@@ -29,7 +34,7 @@ fn to_report(diagnostic_report: std::string::String) -> DiagnosticReport {
     }
 }
 
-fn reduce(diagnostic_report: DiagnosticReport, entry_size: usize) {
+fn calculate_power_consumption(diagnostic_report: DiagnosticReport, entry_size: usize) -> (usize, usize) {
     let report_entry_count: usize = diagnostic_report.values().count();
     let threshold: usize = (report_entry_count / 2) + (report_entry_count % 2);
     let mut gamma_rate: usize = 0;
@@ -40,20 +45,45 @@ fn reduce(diagnostic_report: DiagnosticReport, entry_size: usize) {
         while report_entries.peek().is_some() {
             let x = report_entries.next().unwrap();
             no_of_ones += (x >> n) & 1;
-            
         }
-        println!("Found {} ones at position {}", no_of_ones, n);
-        gamma_rate = gamma_rate + if no_of_ones > threshold { 1<<n } else { 0 };
-        epsilon_rate = epsilon_rate + if no_of_ones < threshold { 1<<n } else { 0 };
-        println!("result is now {}", format!("{:#6b}", gamma_rate));
-        println!("result is now {}", format!("{:#6b}", epsilon_rate));
+        gamma_rate = gamma_rate + if no_of_ones >= threshold { 1<<n } else { 0 };
+        epsilon_rate = epsilon_rate + if no_of_ones <= threshold { 1<<n } else { 0 };
     }
-    println!("gamma rate: {}", gamma_rate);
-    println!("{}", format!("{:#6b}", gamma_rate));
+    (gamma_rate, epsilon_rate)    
+}
 
-    println!("epsilon rate: {}", epsilon_rate);
-    println!("{}", format!("{:#6b}", epsilon_rate));
+fn reduce(report_entries: Vec<usize>, bit_shift: usize, return_largest_set: bool) -> Vec<usize> {
+    let ones: Vec<usize> = report_entries.clone().into_iter().filter(|x| ((x >> bit_shift) & 1) == 1).collect();
+    let zeros: Vec<usize> = report_entries.into_iter().filter(|x| ((x >> bit_shift) & 1) == 0).collect();
+    let ones_count: usize = ones.iter().count();
+    let zeros_count: usize = zeros.iter().count();
+    return match return_largest_set {
+        false => if zeros_count <= ones_count { zeros } else { ones }
+        true => if ones_count >= zeros_count { ones } else { zeros }
+    }
+}
 
-    println!("The answer is {}", epsilon_rate*gamma_rate);
-    
+
+fn calculate_life_support_rating(diagnostic_report: DiagnosticReport, entry_size: usize) {
+    let mut oxygen_report_entries: Vec<usize> = diagnostic_report.items.clone().into_iter().collect();
+    let mut co2_scrubber_report_entries: Vec<usize> = diagnostic_report.items.clone().into_iter().collect();
+    for n in (0..entry_size).rev() {
+        if oxygen_report_entries.iter().count() > 1 {
+            oxygen_report_entries = reduce(oxygen_report_entries, n, true);
+        }
+        if co2_scrubber_report_entries.iter().count() > 1 {
+            co2_scrubber_report_entries = reduce(co2_scrubber_report_entries, n, false);
+        }
+
+        
+        
+    }
+    assert_eq!(oxygen_report_entries.iter().count(), 1);
+    assert_eq!(co2_scrubber_report_entries.iter().count(), 1);
+
+    let oxygen_generator_rating: &usize = oxygen_report_entries.iter().next().unwrap();
+    let co2_scrubber_rating: &usize = co2_scrubber_report_entries.iter().next().unwrap();
+    println!("oxygen_generator_rating: {}", oxygen_generator_rating);
+    println!("co2_scrubber_rating: {}", co2_scrubber_rating);
+    println!("Life time support rating is {}", oxygen_generator_rating*co2_scrubber_rating);
 }
