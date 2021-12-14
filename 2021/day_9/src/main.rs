b9e09dbd5395b491f1d921d2d486d6e60e2600f2
Tim <code@timid.nl>
2021-12-14 08:59:40 +0100

    end of day 9

--- 2021/day_9/src/main.rs
+++ 2021/day_9/src/main.rs
@@ -1,16 +1,24 @@
 use std::env;
 use util::input_operations::{read_file_to_string,split_lines,split_chars};
+use std::collections::HashSet;
 
 #[derive(Debug,Clone)]
 struct HeightMap {
     map_width: usize,
     map_height: usize,
-    locations: Vec<Location>
+    locations: Vec<Location>,
+    basins: Vec<Basin>
+}
+
+#[derive(Debug,Clone,PartialEq,Eq,Hash)]
+struct Location {
+    index: usize,
+    height: usize
 }
 
 #[derive(Debug,Clone)]
-struct Location {
-    height: usize
+struct Basin {
+    locations: HashSet<Location>
 }
 
 impl HeightMap {
@@ -18,7 +26,8 @@ impl HeightMap {
         HeightMap {
             map_width: 0,
             map_height: 0,
-            locations: Vec::new()
+            locations: Vec::new(),
+            basins: Vec::new()
         }
     }
 
@@ -27,19 +36,14 @@ impl HeightMap {
             self.map_width = line.len();
         }
         for line in lines {
-            self.map_height += 1;
             let mut new_locations: Vec<Location> = split_chars(&line.to_string())
                 .filter(|x| x.is_digit(10))
-                .map(|x| Location::new(x.to_digit(10).unwrap() as usize))
+                .enumerate()
+                .map(|(i,x)| Location::new(x.to_digit(10).unwrap() as usize, i + (self.map_height*self.map_width)))
                 .collect::<Vec<Location>>();
             self.locations.append(&mut new_locations);
+            self.map_height += 1;
         }
-    }
-
-    fn get_location(&self, index: usize) -> &Location {
-        assert!(self.map_width > 0);
-        assert!(index < self.map_width*self.map_height);
-        return &self.locations[(index / self.map_width) + (index % self.map_width)]
     }
 
     fn left_neighbour(&self, index: usize) -> Option<&Location> {
@@ -70,42 +74,99 @@ impl HeightMap {
         return Some(&self.locations[index + self.map_width]);
     }
 
+    fn neighbours(&self, index: usize) -> Vec<usize> {
+        let mut neighbour_indices: Vec<usize> = Vec::new();
+        if let Some(left) = self.left_neighbour(index) {
+            neighbour_indices.push(left.index);
+        }
+        if let Some(top) = self.top_neighbour(index) {
+            neighbour_indices.push(top.index);
+        }
+        if let Some(right) = self.right_neighbour(index) {
+            neighbour_indices.push(right.index);
+        }
+        if let Some(bottom) = self.bottom_neighbour(index) {
+            neighbour_indices.push(bottom.index);
+        }
+        neighbour_indices
+    }
+
     fn is_lowest_point(&self, index: usize) -> bool {
         let current_height = self.locations[index].height;
         let mut is_lowest = true;
-        if let Some(left) = self.left_neighbour(index) {
-            is_lowest &= current_height < left.height;
-        }
-        if let Some(top) = self.top_neighbour(index) {
-            is_lowest &= current_height < top.height;
-        }
-        if let Some(right) = self.right_neighbour(index) {
-            is_lowest &= current_height < right.height;
-        }
-        if let Some(bottom) = self.bottom_neighbour(index) {
-            is_lowest &= current_height < bottom.height;
+        for neighbour_idx in self.neighbours(index).iter() {
+            is_lowest &= current_height < self.locations[*neighbour_idx].height;
         }
         is_lowest
     }
 
+    fn lowest_points(&self) -> Vec<usize> {
+        self.locations.iter()
+            .filter(|x| self.is_lowest_point(x.index))
+            .map(|x| x.index)
+            .collect()
+    }
+
     fn risk_level(&self, index: usize) -> usize {
-        if self.is_lowest_point(index) {
-            println!("lowest point is at index {}", index);
-            return &self.locations[index].height + &1;
-        }
-        return 0;
+        return &self.locations[index].height + &1;
     }
 
     fn risk_level_sum(&self) -> usize {
-        self.locations.iter().enumerate().map(|(i,_)| self.risk_level(i)).sum()
+        self.lowest_points()
+            .iter()
+            .map(|i| self.risk_level(*i))
+            .sum()
+    }
+
+    fn find_bassins(&mut self) {
+        self.basins.retain(|_x| true); // Remove all elements
+        for lowest_point in self.lowest_points().into_iter() {
+            let mut basin = Basin::init();
+            self.fill_basin(lowest_point, &mut basin);
+            self.basins.push(basin);
+        }
+    }
+
+    fn find_largest_basins(&mut self) -> usize {
+        self.find_bassins();
+        self.basins
+            .sort_by(|a,b| b.size().cmp(&a.size()));
+        let basin_length = self.basins.iter().len();
+        let largest_basins = self.basins[0..std::cmp::min(3,basin_length)].iter();
+        let mut basin_multiplication = 1;
+        largest_basins.for_each(|x| basin_multiplication *= x.size());
+        return basin_multiplication
+    }
+
+    fn fill_basin(&self, index: usize, basin: &mut Basin) {
+        let current_location = &self.locations[index];
+        if !basin.locations.contains(current_location) && current_location.height < 9 {
+            basin.locations.insert(current_location.clone());
+            for neighbour_index in self.neighbours(index) {
+                self.fill_basin(neighbour_index, basin);
+            }
+        }
     }
 }
 
 impl Location {
-    fn new(height: usize) -> Location {
+    fn new(height: usize, index: usize) -> Location {
         Location {
-            height: height
+            height: height,
+            index: index
         }
+    }
+}
+
+impl Basin {
+    fn init() -> Basin {
+        Basin {
+            locations: HashSet::new()
+        }
+    }
+
+    fn size(&self) -> usize {
+        self.locations.iter().count()
     }
 }
 
@@ -121,5 +182,7 @@ fn main() {
     height_map.read_input(&mut lines);
 
     println!("Risk level: {}", height_map.risk_level_sum());
+
+    println!("Basins: {:?}", height_map.find_largest_basins());
     
 }
