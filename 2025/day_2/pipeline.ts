79c434535c67a417f6b12cf34ca618ac13acd1f5
Raanelom <code@timid.nl>
2025-12-07 15:43:53 +0100

    day 2 complete

--- 2025/day_2/pipeline.ts
+++ 2025/day_2/pipeline.ts
@@ -6,39 +6,45 @@ function numDigits(num: number) {
   return Math.floor(Math.log10(num)) + 1;
 }
 
-function roundToNextEvenNumDigits(num: number) {
-    const base = Math.floor(Math.log10(num));
-    if (base % 2 === 0) {
-        return Math.pow(10, base + 1)
-    }
-    return num;
-}
+// function roundToNextEvenNumDigits(num: number) {
+//     const base = Math.floor(Math.log10(num));
+//     if (base % 2 === 0) {
+//         return Math.pow(10, base + 1)
+//     }
+//     return num;
+// }
 
-function roundToPreviousEvenNumDigits(num: number) {
-    const base = Math.floor(Math.log10(num));
-    if (base <= 0) {
-        return 0;
-    }
-    if (base % 2 === 0) {
-        return Math.pow(10, base) - 1;
-    }
-    return num;
-}
+// function roundToPreviousEvenNumDigits(num: number) {
+//     const base = Math.floor(Math.log10(num));
+//     if (base <= 0) {
+//         return 0;
+//     }
+//     if (base % 2 === 0) {
+//         return Math.pow(10, base) - 1;
+//     }
+//     return num;
+// }
 
 let invalidIds = 0;
 for (const productIdRange of productIdRanges) {
     const [start, end] = productIdRange;
 
-    for(let i = roundToNextEvenNumDigits(start); i <= roundToPreviousEvenNumDigits(end); i++) {
+    for(let i = start; i <= end; i++) {
         const digits = numDigits(i);
-        const firstPart = Math.floor(i / (Math.pow(10, digits / 2)));
-        const secondPart = (firstPart * Math.pow(10, digits / 2));
-        // console.log("\n");
-        // console.log(i);
-        // console.log(firstPart);
-        // console.log(secondPart);
-        invalidIds += i - firstPart === secondPart ? i : 0;
+        for(let j = 1; j <= digits / 2; j++) {
+            const pattern = new RegExp(`^(${Math.floor(i / (Math.pow(10, digits - j)))})+$`);
+
+            if(pattern.test(i.toString())) {
+                // console.log("\nPattern", pattern);
+                // console.log(i);
+                invalidIds += i;
+                break;
+            }
+        }
+        
     }
 }
 
-console.log(invalidIds);
+// Should be 4174379265
+// Is        4174379265
+console.log("\n", invalidIds);
