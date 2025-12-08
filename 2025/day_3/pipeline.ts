3b9566db140874374c6cf0219898c42b24b83d91
Raanelom <code@timid.nl>
2025-12-08 20:39:17 +0100

    string solution

--- 2025/day_3/pipeline.ts
+++ 2025/day_3/pipeline.ts
@@ -24,27 +24,33 @@ const input: string[] = readFileSync('./example_input', 'utf-8').trim().split('\
 //     return chunks;
 // }
 
+
+function maxDigit(no: string, step = 0): { digit: number, position: number } {
+    if (!no.length) {
+        return { digit: 0, position: step };
+    }
+    const remainder = parseInt(no[0]);
+    const nextDigit = maxDigit(no.slice(1), step + 1);
+    if (nextDigit.digit > remainder) {
+        return nextDigit;
+    }
+    return { digit: remainder, position: step };
+}
+
 let joltage = 0;
 for (const no of input) {
     console.log("\nNo", no);
-    const chunks = mapToNumbers(no);
-    const max = maxDigit(no);
-    // Extract the exact digit
-    let subtract = (max.digit * Math.pow(10, max.position))
-    if (max.position > 0) {
-        // Extract all digits beyond the position
-        subtract = Math.floor(no / Math.pow(10, max.position)) * Math.pow(10, max.position);
+    let max = maxDigit(no);
+    let remainder = no.slice(max.position + 1);
+    if (!remainder.length) {
+        console.log("Equal length");
+        max = maxDigit(no.slice(0, -1), 1);
+        remainder = no.slice(max.position);
     }
-    const nextNo = no - subtract;
-    console.log("Nextno", nextNo)
-    const nextMax = maxDigit(nextNo);
-    if (max.position === 0) {
-        joltage += nextMax.digit * 10 + max.digit;
-    }
-    else {
-        joltage += max.digit * 10 + nextMax.digit;
-    }
-    console.log(joltage);
+    const nextMax = maxDigit(remainder);
+    // console.log(max, nextMax);
+    joltage += (max.digit * 10 + nextMax.digit);
+    
 }
 
 console.log(joltage);
