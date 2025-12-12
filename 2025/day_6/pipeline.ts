0edbe0654ecbd2a127c1d1923a306c0798de6f2d
Raanelom <code@timid.nl>
2025-12-12 22:22:37 +0100

    day 6 complete

--- 2025/day_6/pipeline.ts
+++ 2025/day_6/pipeline.ts
@@ -4,7 +4,15 @@ const input: string[] = readFileSync('./input', 'utf-8').trim().split('\n');
 
 
 const operators = input.slice(-1).join("").split(new RegExp(/[ ]+/));
-const numbers = input.slice(0, -1).map((row) => row.trim().split(new RegExp(/[ ]+/)).map((no) => parseInt(no)));
+const numbers = input.slice(0, -1);
+const verticalNumbers: string[] = numbers[0].split("").map((_) => "");
+
+for (let i = 0; i < numbers[0].length; i++) {
+    numbers.forEach((item) => {
+        verticalNumbers[i] += item[i];
+    })
+    verticalNumbers[i] = verticalNumbers[i].trim();
+}
 
 const ADD = "+";
 const MULTIPLY = "*";
@@ -34,13 +42,23 @@ const calc = (operator: string) => {
     }
 }
 
+// Part A
+// let total = 0;
+// for(let i = 0; i < numbers[0].length; i++) {
+//     const selectedRange = numbers.reduce((previous, current) => {
+//         return [...previous, current[i]];
+//     }, []);
+//     total += calc(operators[i])(selectedRange)
+// }
+
 let total = 0;
-for(let i = 0; i < numbers[0].length; i++) {
-    // Use reduce instead of filter here
-    const selectedRange = numbers.reduce((previous, current) => {
-        return [...previous, current[i]];
-    }, []);
+for(let i = 0; i < operators.length; i++) {
+    const selectedRange = verticalNumbers
+        .splice(0, (verticalNumbers.indexOf("") + 1) || verticalNumbers.length)
+        .filter((item) => item !== "")
+        .map((item) => parseInt(item));
     total += calc(operators[i])(selectedRange)
 }
 
+// 3263827
 console.log(total);
