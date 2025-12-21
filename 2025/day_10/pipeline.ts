9ca16af376081d1d709103e882a92ad5c21f4f1a
Raanelom <code@timid.nl>
2025-12-21 11:53:59 +0100

    new direction

--- 2025/day_10/pipeline.ts
+++ 2025/day_10/pipeline.ts
@@ -17,28 +17,91 @@ function sleep(ms: number) {
 // Optimize a + b + c + d + e
 
 const press = async (
-    buttons: number[][], 
-    endState: number[], 
+    buttons: number[][],
+    state: number[] = []
 ): Promise<number> => {
-    const numButtons = buttons.length;
-    const numPositions = endState.length;
-    
-    // Build the matrix where matrix[pos][button] = 1 if button affects pos
-    const matrix: number[][] = Array(numPositions).fill(0).map(() => Array(numButtons).fill(0));
-    
-    for (let b = 0; b < numButtons; b++) {
-        for (const pos of buttons[b]) {
-            matrix[pos][b] = 1;
-        }
+    // Let's implement this algorithm: https://old.reddit.com/r/adventofcode/comments/1pk87hl/2025_day_10_part_2_bifurcate_your_way_to_victory/
+    if (state.every((num) => num === 0)) {
+        // State is empty? We're done
+        return 0;
     }
+    
+    const oddPositions = state
+        .map((val, index) => ({ index, isOdd: val % 2 === 1 }))
+        .filter((val) => val.isOdd)
+        .map((val) => val.index);
+
+    console.log("State", state);
+    console.log("Odd positions", oddPositions);
+    
+    return 0;
     // Solve matrix, dive into study material
 };
+
+const pressBoolean = async (remaining: number[][], endState: boolean[]): Promise<number> => {
+    // console.log("Buttons to press", remaining.length);
+    const queue: {
+        state: boolean[];
+        pressed: number[][];
+        remaining: number[][];
+    }[] = [];
+
+    const emptyState: boolean[] = new Array<boolean>(endState.length).fill(false);
+
+    queue.push({
+        state: emptyState,
+        pressed: [],
+        remaining
+    });
+
+    const knownStates: Set<string> = new Set();
+
+    while (queue.length) {
+        const current = queue.shift();
+        if (!current) {
+            throw new Error("Queue has length, but is also empty?");
+        }
+
+        if (JSON.stringify(current.state) === JSON.stringify(endState)) {
+            // This is the first viable solution
+            return current.pressed.length;
+        }
+
+        if (!current.remaining.length) {
+            // We can continue, this state is not worth investigating
+            continue;
+        }
+
+        if (knownStates.has(JSON.stringify(current.pressed))) {
+            // We already processed this state, continue
+            continue;
+        }
+
+        knownStates.add(JSON.stringify(current.pressed))
+
+        for (let i = 0; i < current.remaining.length; i++) {
+            const next = structuredClone(current);
+            const buttonToPress = next.remaining.splice(i, 1)[0];
+
+            next.pressed.push(buttonToPress);
+
+            for (const b of buttonToPress) {
+                next.state[b] = !next.state[b];
+            }
+
+            next.pressed.sort();
+            queue.push(next);
+        }
+    }
+
+    return Infinity;
+}
 
 const input: {
     lightState: boolean[];
     buttons: number[][];
     joltageState: number[];
-}[] = readFileSync('./input', 'utf-8')
+}[] = readFileSync('./example_input', 'utf-8')
     .trim()
     .split('\n')
     .map((line: string) => {
