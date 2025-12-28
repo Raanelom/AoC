ea3ab595cab0d894fa37f69d66b38f8f1cfd27e1
Raanelom <code@timid.nl>
2025-12-28 18:56:00 +0100

    still not efficient

--- 2025/day_11/pipeline.ts
+++ 2025/day_11/pipeline.ts
@@ -1,28 +1,85 @@
 import { readFileSync } from 'fs';
 
-const findPaths = (from: string, 
-        edges: Map<string, string[]>, 
-        path: string = from, 
-        paths: Set<string> = new Set()) => {
-    if (path.endsWith("out")) {
-        paths.add(path);
-        return;
+const getSortedIndegree = (edges: Map<string, string[]>) => {
+    // Let's do some topological sort first
+    let indegree = new Map<string, number>();
+    // Set nodes without indegree to zero
+    [...edges.keys()].filter((node) => {
+        return !([...edges.values()].some((nodeList) => nodeList.includes(node)));
+    }).forEach((key) => {
+        indegree.set(key, 0);
+    });
+    for (const edge of edges) {
+        for (const target of edge[1]) {
+            indegree.set(target, (indegree.get(target) || 0) + 1)
+        }
     }
-    const possiblePaths = edges.get(from);
-    if (!possiblePaths || !possiblePaths.length) {
-        return;
+    const sortedIndegree = [...indegree.entries()].sort((a, b) => a[1] - b[1]);
+
+    return new Map(sortedIndegree);
+}
+
+// Inspired by https://www.geeksforgeeks.org/dsa/count-possible-paths-two-vertices/#approach-1-using-depthfirst-search-o2n-time-and-on-space
+const findPaths = (
+    from: string,
+    to: string,
+    edges: Map<string, string[]>
+) => {
+    // Let's do some topological sort first
+    const indegree = getSortedIndegree(edges);
+
+    const order: string[] = [];
+    // Start with indegree 0
+    const queue = [...indegree.entries()].filter((n) => n[1] === 0).map((n) => n[0]);
+
+    while (queue.length > 0) {
+        const node = queue.shift()!;
+        order.push(node);
+
+        if (!edges.has(node)) {
+            continue;
+        }
+        for (const next of edges.get(node)!) {
+            indegree.set(next, indegree.get(next)! - 1);
+            if (indegree.get(next) === 0) {
+                queue.push(next);
+            }
+        }
     }
-    for (const nextPath of possiblePaths) {
-        console.log(path);
-        findPaths(nextPath, edges, `${path} ${nextPath}`, paths);
+
+    const reversePaths = new Map<string, string[]>();
+    reversePaths.set(from, [from]);
+
+    const noOfPaths = new Map<string, number>();
+    noOfPaths.set(from, 1);
+
+    for (const node of order) {
+        const prev = reversePaths.get(node) || [];
+        if (edges.has(node)) {
+            for (const next of edges.get(node)!) {
+                noOfPaths.set(next, (noOfPaths.get(next) || 0) + (noOfPaths.get(node) || 0));
+                // console.log(node, next);
+                const nextPaths = reversePaths.get(next) || [];
+                for (const prevPath of prev) {
+                    // console.log(prevPath);
+                    if (next !== "out" || (prevPath.includes("fft") && prevPath.includes("dac"))) {
+                        nextPaths?.push(prevPath + " " + next);
+                    }
+                }
+                reversePaths.set(next, nextPaths);
+            }
+            // console.log("delete");
+            reversePaths.delete(node);
+        }
     }
-    return paths;
+    console.log(noOfPaths.get(to));
+    return reversePaths.get(to);
 }
 
 const exec = () => {
     const inputLines: string[] = readFileSync('./input', 'utf-8').trim().split('\n');
 
-    const edges = new Map<string, string[]>;
+    const edges = new Map<string, string[]>();
 
     inputLines.forEach((line) => {
         const [ source, targets ] = line.split(": ");
@@ -32,12 +89,25 @@ const exec = () => {
         }
     });
 
+    const sortedEdges = new Map([...edges.entries()].sort((a, b) => a[1].length - b[1].length));
+
     // const start = edges.get("you");
 
-    console.log(edges);
+    // console.log(edges);
     // console.log(start);
-    const paths = findPaths("you", edges);
-    console.log(paths?.size);
+    // Ideas:
+    // - Multiple "find shortest path"-algorithm, excluding previously found shortest paths
+    // - Bread-first-search
+    const paths = findPaths("svr", "out", sortedEdges);
+    // const pathsToFFT = findPaths("svr", "fft", sortedEdges);
+    // const pathsToDAC = findPaths("svr", "dac", sortedEdges);
+    // const pathsFromFFT = findPaths("fft", "out", sortedEdges);
+    // const pathsFromDAC = findPaths("dac", "out", sortedEdges);
+    console.log(paths);
+    // console.log("Paths to fft", pathsToFFT);
+    // console.log("Paths to dac", pathsToDAC);
+    // console.log("Paths from fft", pathsFromFFT);
+    // console.log("Paths from dac", pathsFromDAC);
 }
 
 exec();
